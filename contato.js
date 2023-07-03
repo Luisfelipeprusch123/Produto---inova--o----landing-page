df6c9c238baa5e6d6392b9e511f7284eef3423bc
Luisfelipeprusch123 <103533135+Luisfelipeprusch123@users.noreply.github.com>
2023-07-03 20:14:04 -0300

    Atualização nos contatos

---
+++ contato.js
@@ -0,0 +1,4 @@
+// Adiciona uma classe "responsive" à navbar ao clicar no ícone do menu
+document.querySelector('.nav-links').addEventListener('click', function () {
+  this.classList.toggle('responsive');
+});
