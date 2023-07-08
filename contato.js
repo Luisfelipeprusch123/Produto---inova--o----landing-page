b79bc3782571b42841a84a6437017de096b8abe4
Luisfelipeprusch123 <103533135+Luisfelipeprusch123@users.noreply.github.com>
2023-07-08 14:06:23 -0300

    Atualização de form

--- contato.js
+++ contato.js
@@ -5,33 +5,52 @@ document.querySelector('.nav-links').addEventListener('click', function () {
 
 
 // Envio de dados pelo E-mail
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-  event.preventDefault(); // Impede o envio padrão do formulário
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const port = 3000;
 
-  var form = event.target;
-  var formData = new FormData(form);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-  fetch("https://formspree.io/luisfelipejmachadopruch@gmail.com", {
-      method: "POST",
-      headers: {
-          "Accept": "application/json"
-      },
-      body: formData
-  })
-  .then(function(response) {
-      return response.json();
-  })
-  .then(function(data) {
-      // Lida com a resposta do servidor
-      if (data.success) {
-          alert("Formulário enviado com sucesso!");
-          form.reset(); // Limpa o formulário após o envio
-      } else {
-          alert("Ocorreu um erro ao enviar o formulário.");
-      }
-  })
-  .catch(function(error) {
-      console.log("Erro:", error);
+app.post('/submit', (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Configurar o transporte de e-mail
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'luisfelipejmachadopruch@gmail.com', // Substitua pelo seu e-mail
+      pass: 'Luisfelipe8465' // Substitua pela sua senha
+    }
+  });
+
+  // Configurar as opções de e-mail
+  const mailOptions = {
+    from: 'luisfelipejmachadopruch@gmail.com', // Substitua pelo seu e-mail
+    to: 'luisfelipejmachadopruch@gmail.com', // Substitua pelo e-mail do destinatário
+    subject: 'Novo formulário de contato',
+    text: `
+      Nome: ${name}
+      E-mail: ${email}
+      Mensagem: ${message}
+    `
+  };
+
+  // Enviar o e-mail
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log('Erro ao enviar o e-mail:', error);
+      res.status(500).json({ success: false });
+    } else {
+      console.log('E-mail enviado:', info.response);
+      res.json({ success: true });
+    }
   });
 });
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
 
